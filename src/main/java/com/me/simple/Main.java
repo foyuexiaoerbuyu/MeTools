251af51a39af0df2ebb2354097e6dd4f03e2bab0
米伟强 <1950053842@qq.com>
2023-10-12 17:18:37 +0800

    测试打包

--- src/main/java/com/me/simple/Main.java
+++ src/main/java/com/me/simple/Main.java
@@ -1,11 +1,8 @@
 package com.me.simple;
 
-import org.yifan.hao.WinUtils;
-
 import java.awt.*;
 
 public class Main {
     public static void main(String[] args) throws AWTException {
-        WinUtils.showMousePos();
     }
 }
