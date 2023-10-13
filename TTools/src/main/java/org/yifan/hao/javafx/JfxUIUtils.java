c69e4195374caf56405f920821648dcf01f9f305
子非鱼 <1950053842@qq.com>
2023-10-13 18:00:45 +0800

    重命名JfxUIUtils.java

--- TTools/src/main/java/org/yifan/hao/javafx/JfxUIUtils.java
+++ TTools/src/main/java/org/yifan/hao/javafx/JfxUIUtils.java
@@ -2,7 +2,7 @@ package org.yifan.hao.javafx;
 
 import javafx.application.Platform;
 
-public class UIUtils {
+public class JfxUIUtils {
 
     public static void runOnUIThread(Runnable r) {
         Platform.runLater(r);
